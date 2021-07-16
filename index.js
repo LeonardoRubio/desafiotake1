60ea9e9cac084ec00c52fcba66dadca9bfd6662a
Leonardo Rubio <leonardo.fmedeiros@hotmail.com>
2021-07-16 11:24:20 -0300

    atualizado

--- index.js
+++ index.js
@@ -6,9 +6,28 @@ const port = process.env.PORT || 3000
 app.use(express.json())
 app.get("/desafiotake", async (req, res) => {
     try {
-        const { data } = await axios.get("https://api.github.com/users/takenet/repos")
+        const { data } = await axios.get("https://api.github.com/users/takenet/repos?sort=created&direction=asc")
         const formatado = data.filter(repos => repos.language == "C#")
-        return res.json(formatado)
+        const busca = []
+
+        let count = 1
+        let teste = formatado.map(items => {
+            if (count <= 5) {
+                count++
+                busca.push({
+                    name: items.name,
+                    description: items.description,
+                    avatar_url: items.owner.avatar_url,
+                })
+            }
+        })
+
+        console.log(formatado)
+       
+        console.log(busca)
+        return res.json(busca)
+
+        
 
 
 
